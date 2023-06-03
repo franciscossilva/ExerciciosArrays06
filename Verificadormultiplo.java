46034cf6258e5bdf82908cbfcb7df21453faf25f
franciscossilva <francisco.dossantos@zup.com.br>
2023-06-02 23:12:30 -0300

    Initial commit

---
+++ Verificadormultiplo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Verificadormultiplo {public static void main (String[]args){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite o numero inteiro : ");
+    int mumero = scanner.nextInt();
+    if (numero % 7 == 0 && numero % 5 == 0) {
+        System.out.println("o numero  " + numero + " é multiplo de 7 e 5.");
+    }  else  {
+        System.out.println("o numero : "+ numero + "nao é multiplo de 7 e 5")
+    }}}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
